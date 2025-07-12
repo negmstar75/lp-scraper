ef309d9eaa419bd290a0eef3b582fbfbce526d4e
negmstar75 <negmstar@gmail.com>
2025-07-12 15:04:58 +0300

    Update scrape.js

--- pages/api/scrape.js
+++ pages/api/scrape.js
@@ -19,9 +19,6 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  if (!verifyRequest(req)) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
 
   const slugParam = req.query.slug || '';
   const mockData = getMockDestinations();
